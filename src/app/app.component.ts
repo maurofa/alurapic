e5aea93cf7a3d97cdfcbe284e36c3a2e49858506
Mauro Farias Brito <maurofabri@gmail.com>
2019-09-24 09:51:42 -0300

    atualizações até o capítulo 3 do curso.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo.service';
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'alurapic';
+export class AppComponent implements OnInit {
+  photos: Photo[] = [];
+
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+    this.photoService
+      .listaDoUsuario('flavio')
+      .subscribe(photos => this.photos = photos);
+  }
 }
